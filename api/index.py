7607eacac37f79ee9c2deb62e56f9780924fafde
AJE71096 <88673726+AJE71096@users.noreply.github.com>
2024-07-08 16:18:23 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,36 +2,31 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from src.chatgpt import ChatGPT
+from .chatgpt import ChatGPT
 
 import os
 
 line_bot_api = LineBotApi(os.getenv("4lNwqrWgkV1qsiH0SjKKl3VePxeGi/nf/ztjQOnBHyWZiblpK9ErXUOGKaU/r82xYZ1fZB3sGIdH8Gg2WTUuEuYRbw9mS2NOoZieKBJvIbOxAQeX8IqMk2M1qBiw6bTV2FFgS1HcFCUuRRaWk3NXlgdB04t89/1O/w1cDnyilFU="))
 line_handler = WebhookHandler(os.getenv("2e0af199060c83222d4c636805134a71"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+working_status = os.getenv("DEFALUT_TALKING", default="true").lower() == "true"
 
 app = Flask(__name__)
 chatgpt = ChatGPT()
 
-# domain root
 @app.route('/')
 def home():
     return 'Hello, World!'
 
 @app.route("/webhook", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -61,7 +56,6 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
 
 if __name__ == "__main__":
     app.run()
