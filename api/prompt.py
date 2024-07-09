58a90763f14ef77dec06d75d195ec1e2e2e384da
AJE71096 <88673726+AJE71096@users.noreply.github.com>
2024-07-09 16:33:47 +0800

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -1,11 +1,11 @@
 import os
 
-chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
+chat_language = os.getenv("INIT_LANGUAGE", default="zh")
 
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 50))
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default="50"))
 LANGUAGE_TABLE = {
-  "zh": "哈囉！",
-  "en": "Hello!"
+    "zh": "哈囉！",
+    "en": "Hello!"
 }
 
 AI_GUIDELINES = '你是一個AI資訊助手，會簡單明瞭的提供用戶想要的資訊'
@@ -13,11 +13,11 @@ AI_GUIDELINES = '你是一個AI資訊助手，會簡單明瞭的提供用戶想�
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append(
-            {
-                "role": "system", 
-                "content": f"{LANGUAGE_TABLE[chat_language]}, {AI_GUIDELINES})"
-             })    
+        self.msg_list.append({
+            "role": "system", 
+            "content": f"{LANGUAGE_TABLE[chat_language]}, {AI_GUIDELINES}"
+        })
+    
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
             self.msg_list.pop(0)
