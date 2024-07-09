51ace096470c1e58b464d647f5388d13aa969e07
AJE71096 <88673726+AJE71096@users.noreply.github.com>
2024-07-09 09:40:55 +0800

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -2,26 +2,24 @@ import os
 
 chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
 
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 7))
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
 LANGUAGE_TABLE = {
-  "zh": "哈囉！",
-  "en": "Hello!"
+  "zh": "嗨！",
+  "en": "Hi!"
 }
-
-AI_GUIDELINES = '你是一個AI助教，會用蘇格拉底教學法代替老師初步回應，如果有需要會提醒學生跟老師確認'
 
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append(
-            {
-                "role": "system", 
-                "content": f"{LANGUAGE_TABLE[chat_language]}, {AI_GUIDELINES})"
-             })    
+        self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
+    
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
-            self.msg_list.pop(0)
-        self.msg_list.append({"role": "user", "content": new_msg})
+            self.remove_msg()
+        self.msg_list.append(new_msg)
+
+    def remove_msg(self):
+        self.msg_list.pop(0)
 
     def generate_prompt(self):
-        return self.msg_list
+        return '\n'.join(self.msg_list)
