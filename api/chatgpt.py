1322a0db995a8cba0c5a645043911f08b935fa58
AJE71096 <88673726+AJE71096@users.noreply.github.com>
2024-07-09 10:10:41 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -1,25 +1,30 @@
-import os
-from openai import OpenAI
 from api.prompt import Prompt
 
-client = OpenAI()
-client.api_key = os.getenv("OPENAI_API_KEY")
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-4o")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=500))
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4o")
+        #self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
 
     def get_response(self):
-        response = client.chat.completions.create(
+        response = openai.Completion.create(
             model=self.model,
-            messages=self.prompt.generate_prompt(),
+            prompt=self.prompt.generate_prompt(),
             temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        return response.choices[0].message.content
+        return response['choices'][0]['text'].strip()
 
-    def add_msg(self, role, text):
-        self.prompt.add_msg(role, text)
+    def add_msg(self, text):
+        self.prompt.add_msg(text)
